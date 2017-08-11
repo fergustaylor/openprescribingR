e8a3d21c5a20a62ca242a4d1ee90a49520464690
Fergus Taylor <fergustay@gmail.com>
2017-08-11 19:17:22 +0100

    package stuff

---
+++ R/openprescribingR.R
@@ -0,0 +1,19 @@
+#' OpenprescribingR: A small package for OpenPrescribing data directly.
+#' 
+#' @section openprescribingR Functions:
+#' 
+#' * drug_details(name = NULL, chemical_or_presentation_code = NULL, BNF_section_code = NULL, exact_name_or_code = NULL)
+#' 
+#' * organisation_codes(organisation_code_or_name = NULL, CCG_code_or_name = NULL, practice_code_or_name = NULL, exact_name_or_code = NULL)
+#' 
+#' * list_size(list_size_by_code = NULL, ASTRO_PU_by_code = NULL)
+#' 
+#' * spending_by_CCG(chemical_code = NULL, CCG_code = NULL)
+#' 
+#' * spending_by_code(BNF_code = NULL)
+#' 
+#' * spending_by_practice(BNF_section_code = NULL, chemical_code = NULL, presentation_code = NULL, practice_code = NULL, CCG_code = NULL, date_code = NULL)
+#' 
+#' @docType package
+#' @name openprescribingR
+NULL
