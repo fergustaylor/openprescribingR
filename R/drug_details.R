e88bcc1bc1a3852799a57e4df413fe65a01a2a06
Fergus Taylor <fergustay@gmail.com>
2017-08-13 14:59:27 +0100

    changed bnf_code to bnf_section (as in, exact section not id) changed chemical_or_presentation_code back to chemical_section_or_presentation_code (as in, exact id for either of the 3)

--- R/drug_details.R
+++ R/drug_details.R
@@ -1,24 +1,23 @@
 #'Search for the official name and code of BNF sections, chemicals and presentations.
 #'
-#' @param name A code found using drug_details().
-#' @param chemical_or_presentation_code A code found using drug_details().
-#' @param BNF_section_code A code found using drug_details().
-#' @param exact_name_or_code Search by an exact name/code.
-#' @return Returns official names and codes of BNF sections, chemicals and presentations
+#' @param name Drug name.
+#' @param chemical_section_or_presentation_code An ID unique to BNF sections, chemicals or presentations.
+#' @param BNF_section The BNF section.
+#' @param exact_name_or_code Search by an exact name/code(ID).
+#' @return Returns official names and codes(ID) of BNF sections, chemicals and presentations.
 #' @importFrom magrittr "%>%"
 #' @export
 #' @examples
 #' All BNF sections, chemicals and presentations matching a name (case-insensitive) = drug_details(name= "lipid")
-#' All chemicals and presentations matching a code = drug_details(chemical_or_presentation_code= "...")
-#' All BNF sections matching a code = drug_details(BNF_section_code= "...")
-#' All BNF sections matching a code = drug_details(BNF_section_code= "...")
-#' All BNF sections, chemicals and presentations exactly matching a name or code = drug_details(exact_name_or_code= "...")
-#' Or a variation of the above (with at least 1 name, chemical_or_presentation_code, BNF_section_code, or exact_name_or_code).
+#' All chemicals and presentations matching a code = drug_details(chemical_section_or_presentation_code= "0212000AA")
+#' All BNF sections matching a code = drug_details(BNF_section= "2.4")
+#' All BNF sections, chemicals and presentations exactly matching a name or code = drug_details(exact_name_or_code= "212000AA")
+#' Or a variation of the above (with at least 1 name, chemical_section_or_presentation_code, BNF_section, or exact_name_or_code).
 #' N.B For now the use of multiple terms in one category requires "&q=", e.g drug_details(name="lipid&q=drug")
-drug_details <- function(name = NULL, chemical_or_presentation_code = NULL, BNF_section_code = NULL, exact_name_or_code = NULL){
+drug_details <- function(name = NULL, chemical_section_or_presentation_code = NULL, BNF_section = NULL, exact_name_or_code = NULL){
   if (!is.null(name)){variablesegment1 <- stringr::str_c("&q=", name)}
-  if (!is.null(chemical_or_presentation_code)){variablesegment2 <- stringr::str_c("&q=", chemical_or_presentation_code)} 
-  if (!is.null(BNF_section_code)){variablesegment3 <- stringr::str_c("&q=", BNF_section_code)}
+  if (!is.null(chemical_section_or_presentation_code)){variablesegment2 <- stringr::str_c("&q=", chemical_section_or_presentation_code)} 
+  if (!is.null(BNF_section)){variablesegment3 <- stringr::str_c("&q=", BNF_section)}
   if (!is.null(exact_name_or_code)){variablesegment4 <- stringr::str_c("&q=", exact_name_or_code, "&exact=true")}
   variablesegment <- stringr::str_c(
     if(exists("variablesegment1")){variablesegment1},
--- R/openprescribingR.R
+++ R/openprescribingR.R
@@ -2,17 +2,17 @@
 #' 
 #' @section openprescribingR Functions:
 #' 
-#' * drug_details(name = NULL, chemical_or_presentation_code = NULL, BNF_section_code = NULL, exact_name_or_code = NULL)
+#' * drug_details(name = NULL, chemical_section_or_presentation_code = NULL, BNF_section = NULL, exact_name_or_code = NULL)
 #' 
 #' * organisation_codes(organisation_code_or_name = NULL, CCG_code_or_name = NULL, practice_code_or_name = NULL, exact_name_or_code = NULL)
 #' 
 #' * list_size(list_size_by_code = NULL, ASTRO_PU_by_code = NULL)
 #' 
-#' * spending_by_CCG(chemical_code = NULL, CCG_code = NULL)
+#' * spending_by_CCG(chemical_section_or_presentation_code = NULL, CCG_code = NULL)
 #' 
 #' * spending_by_code(BNF_code = NULL)
 #' 
-#' * spending_by_practice(BNF_section_code = NULL, chemical_code = NULL, presentation_code = NULL, practice_code = NULL, CCG_code = NULL, date_code = NULL)
+#' * spending_by_practice(BNF_section = NULL, chemical_code = NULL, presentation_code = NULL, practice_code = NULL, CCG_code = NULL, date_code = NULL)
 #' 
 #' * CCG_boundaries_or_location(CCG_code = NULL, practice_code = NULL)
 #' @docType package
