96a66800c756b34113d329988d3b148caf994521
Fergus Taylor <fergustay@gmail.com>
2017-08-23 10:32:01 +0100

    added an as_sf function to CCG_boundaries_or_location()

--- R/CCG_boundaries_or_location.R
+++ R/CCG_boundaries_or_location.R
@@ -10,13 +10,24 @@
 #' Boundaries of all CCGs = CCG_boundaries_or_location()
 #' Boundaries of an individual CCG = CCG_boundaries_or_location(CCG_code = "99H")
 #' Location (approximate) of a practice, or practices in a CCG, by code = CCG_boundaries_or_location(CCG_code = "99H", practice_code = "P87003")
+#' CCG_boundaries_or_location(as_sf = TRUE)
+#' CCG_boundaries_or_location(CCG_code = "99H", as_sf = TRUE)
+#' CCG_boundaries_or_location(CCG_code = "99H", practice_code = "P87003", as_sf = TRUE)
+#' CCG_boundaries_or_location(practice_code = "P87003", as_sf = TRUE)
 #' Read the [wiki](https://github.com/fergustaylor/openprescribingR/wiki) for more help.
-CCG_boundaries_or_location <- function(CCG_code = NULL, practice_code = NULL){
+CCG_boundaries_or_location <- function(CCG_code = NULL, practice_code = NULL, as_sf = TRUE/FALSE) {
   if (is.null(CCG_code) & is.null(practice_code)) {variablesegment <- stringr::str_c("org_type=ccg")}
   if (!is.null(CCG_code) & is.null(practice_code)) {variablesegment <- stringr::str_c("org_type=ccg&q=", CCG_code)}
   if (is.null(CCG_code) & !is.null(practice_code)) {variablesegment <- stringr::str_c("q=", practice_code)}
   if (!is.null(CCG_code) & !is.null(practice_code)) {variablesegment <- stringr::str_c("q=", CCG_code, ",", practice_code)}
-  stringr::str_c("https://openprescribing.net/api/1.0/", "org_location/?", variablesegment) %>%
-    readLines() %>%
-    rjson::fromJSON()
+  
+  if (as_sf == TRUE) {
+    stringr::str_c("https://openprescribing.net/api/1.0/", "org_location/?", variablesegment) %>% 
+      sf::st_read() %>% 
+      sf::st_as_sf()}
+  
+  else {
+    stringr::str_c("https://openprescribing.net/api/1.0/", "org_location/?", variablesegment) %>%
+      readLines() %>%
+      rjson::fromJSON()}  
 }
